5506f52d238d0ce099ced57128f376bebba80c8f
Emmy Niyonsaba <emmmyson5ku@gmail.com>
2026-05-12 21:00:01 +0200

    done

--- routers/userRouters.js
+++ routers/userRouters.js
@@ -1,12 +1,20 @@
-import express from 'express';
-import { getAllUsers, getUserById, createUser, updateUser, deleteUser } from '../controllers/userControler.js';
+import express from "express";
+import {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+} from "../controllers/userControler.js";
 
 const router = express.Router();
-
-router.get('/users', getAllUsers);
-router.get('/user/:id', getUserById);
-router.post('/user', createUser);
-router.put('/user/:id', updateUser);
-router.delete('/user/:id', deleteUser);
-
+//
+router.get("/users", getAllUsers);
+router.get("/user/:id", getUserById);
+router.post("/user", createUser);
+router.put("/user/:id", updateUser);
+router.delete("/user/:id", deleteUser);
+// router.get('/users', (req, res) => {
+//     res.send('Get all users');
+// });
 export default router;
